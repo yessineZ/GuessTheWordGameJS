536b8e53513eae7074449313fcf53b8e70e02368
Yessine Zouari <145301341+yessineZ@users.noreply.github.com>
2024-04-19 00:21:02 +0100

    Update guess.js

--- guess.js
+++ guess.js
@@ -5,13 +5,13 @@ document.querySelector("h1").innerHTML = gameName;
 let date = new Date() ; 
 document.querySelector("footer").innerHTML = `${gameName} Game created by yessine in ${date.getFullYear()}`;
 
-// Setting Game Options
+
 let numbersOfTries = 6;
 let numbersOfLetters = 6;
 let currentTry = 1;
 let numberOfHints = 2;
 
-// Manage Words
+
 let wordToGuess = "";
 const words = ["Create", "Update", "Delete", "Master", "Branch", "Mainly", "create", "create"];
 wordToGuess = words[Math.floor(Math.random() * words.length)].toLowerCase();
@@ -25,7 +25,7 @@ getHintButton.addEventListener("click", getHint);
 function generateInput() {
   const inputsContainer = document.querySelector(".inputs");
 
-  // Create Main Try Div
+ 
   for (let i = 1; i <= numbersOfTries; i++) {
     const tryDiv = document.createElement("div");
     tryDiv.classList.add(`try-${i}`);
@@ -33,7 +33,6 @@ function generateInput() {
 
     if (i !== 1) tryDiv.classList.add("disabled-inputs");
 
-    // Create Inputes
     for (let j = 1; j <= numbersOfLetters; j++) {
       const input = document.createElement("input");
       input.type = "text";
@@ -44,25 +43,23 @@ function generateInput() {
 
     inputsContainer.appendChild(tryDiv);
   }
-  // Focus On First Input In First Try Element
+
   inputsContainer.children[0].children[1].focus();
 
-  // Disable All Inputs Except First One
   const inputsInDisabledDiv = document.querySelectorAll(".disabled-inputs input");
   inputsInDisabledDiv.forEach((input) => (input.disabled = true));
 
   const inputs = document.querySelectorAll("input");
   inputs.forEach((input, index) => {
-    // Convert Input To Uppercase
+  
     input.addEventListener("input", function () {
       this.value = this.value.toUpperCase();
-      // console.log(index);
+   
       const nextInput = inputs[index + 1];
       if (nextInput) nextInput.focus();
     });
 
-    input.addEventListener("keydown", function (event) {
-      // console.log(event);
+    i
       const currentIndex = Array.from(inputs).indexOf(event.target); // Or this
       // console.log(currentIndex);
       if (event.key === "ArrowRight") {
@@ -90,12 +87,12 @@ function handleGuesses() {
     const letter = inputField.value.toLowerCase();
     const actualLetter = wordToGuess[i - 1];
 
-    // Game Logic
+   
     if (letter === actualLetter) {
-      // Letter Is Correct And In Place
+    
       inputField.classList.add("yes-in-place");
     } else if (wordToGuess.includes(letter) && letter !== "") {
-      // Letter Is Correct And Not In Place
+      
       inputField.classList.add("not-in-place");
       successGuess = false;
     } else {
@@ -115,7 +112,7 @@ function handleGuesses() {
     let allTries = document.querySelectorAll(".inputs > div");
     allTries.forEach((tryDiv) => tryDiv.classList.add("disabled-inputs"));
 
-    // Disable Guess Button
+
     guessButton.disabled = true;
     getHintButton.disabled = true;
   } else {
@@ -151,17 +148,15 @@ function getHint() {
   }
 
   const enabledInputs = document.querySelectorAll("input:not([disabled])");
-  // console.log(enabledInputs);
+ 
   const emptyEnabledInputs = Array.from(enabledInputs).filter((input) => input.value === "");
-  // console.log(emptyEnabledInputs);
+ 
 
   if (emptyEnabledInputs.length > 0) {
     const randomIndex = Math.floor(Math.random() * emptyEnabledInputs.length);
     const randomInput = emptyEnabledInputs[randomIndex];
     const indexToFill = Array.from(enabledInputs).indexOf(randomInput);
-    // console.log(randomIndex);
-    // console.log(randomInput);
-    // console.log(indexToFill);
+
     if (indexToFill !== -1) {
       randomInput.value = wordToGuess[indexToFill].toUpperCase();
     }
@@ -172,7 +167,7 @@ function handleBackspace(event) {
   if (event.key === "Backspace") {
     const inputs = document.querySelectorAll("input:not([disabled])");
     const currentIndex = Array.from(inputs).indexOf(document.activeElement);
-    // console.log(currentIndex);
+  
     if (currentIndex > 0) {
       const currentInput = inputs[currentIndex];
       const prevInput = inputs[currentIndex - 1];
